524d940e2dfbc7b630f13fcc6abeee98ea71ca50
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-05-31 14:20:08 +0500

    Update help.py

--- hikka/modules/help.py
+++ hikka/modules/help.py
@@ -334,7 +334,7 @@ class HelpMod(loader.Module):
             if not self.lookup("Loader")._fully_loaded
             else ""
         )
-        sosi = "🏙️ <b>Heya, Honey!</b> suck me if you want. (√°v°)√"
+        sosi = "\n\n🏙️ <b>Heya, Honey!</b> suck me if you want. (√°v°)√"
         await utils.answer(
             message,
             f"{reply}\n{''.join(core_)}{''.join(plain_)}{''.join(inline_)}{partial_load}" + sosi,
