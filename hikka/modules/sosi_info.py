b2f88808cf735454756114ffe453e4a03fdb7383
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-06-06 09:18:57 +0500

    Update sosi_info.py

--- hikka/modules/sosi_info.py
+++ hikka/modules/sosi_info.py
@@ -118,7 +118,7 @@ class InfomodMod(loader.Module):
                 f'<b>🌉 {self.strings("owner")}: </b>{me}\n'
                 f"<b>🌉 {self.strings('version')}: </b><code>{version}</code> <a href='{build}'></a>\n"
                 f"<a href='{upd}'></a>\n"
-                f"🏙️ <b>Eynshteyn teoriyasi:</b> <code>Akangda shunaqa</code Sosi-Userbot <code> tursayu, siz boshqasini ishlatsangiz ajablanmayman.</code> (◕ᴗ◕✿) sosi <a href='{self.strings('prefix')} {prefix}'></a>\n"
+                f"🏙️ <b>Eynshteyn teoriyasi:</b> <code>Akangda shunaqa</code> Sosi-Userbot <code> tursayu, siz boshqasini ishlatsangiz ajablanmayman.</code> (◕ᴗ◕✿) sosi <a href='{self.strings('prefix')} {prefix}'></a>\n"
                 f"<a href='{platform}'></a>\n"
             )
         )
