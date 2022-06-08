24cf6b47e68752e1ea117e936b76fce5ec606f8c
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-06-08 09:56:06 +0500

    Update sosi_info.py

--- hikka/modules/sosi_info.py
+++ hikka/modules/sosi_info.py
@@ -87,8 +87,8 @@ class InfomodMod(loader.Module):
             if self.config["custom_message"] and self.config["custom_message"] != "no"
             else (
                 "<b>◍ Sosi-Userbot (◕ᴗ◕✿) sosi</b>\n"
-                f'<b>◍ {self.strings("owner")}: </b>{me}\n'
-                f"<b>◍ {self.strings('version')}: </b><code>{version}</code> <a href='{build}'></a>\n"
+                f'<b>◍ {self.strings("owner")}:</b> <a href="{me}">bu yerda</a>\n'
+                f"<b>◍ {self.strings('version')}:</b> <code>{version}</code> <a href='{build}'></a>\n"
                 f"\n◍ ╔═══╦═══╦═══╦══╗"
                 f"\n◍ ║╔═╗║╔═╗║╔═╗╠╣╠╝"
                 f"\n◍ ║╚══╣║─║║╚══╗║║   ╔╗──╔╗"
